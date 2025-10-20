686f41b8f3b0b287d80fabcc64047674321c481f
CCollins1016 <ccollins@bsd220.org>
2025-10-20 11:01:20 -0500

    Initial Commit

---
+++ Roll The Dice Remade/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Roll The Dice Remade
+//
+//  Created by Christian Collins on 10/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Dice Roll")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roll The Dice Remade/Roll_The_Dice_RemadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_The_Dice_RemadeApp.swift
+//  Roll The Dice Remade
+//
+//  Created by Christian Collins on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Roll_The_Dice_RemadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
